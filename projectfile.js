e65bba8c4b40f6c68526cdbde212270fdfad2b1d
Carlos Rivas <Cjrivas00@gmail.com>
2022-02-25 22:02:55 -0500

    added to local fs added functions for readme creating promt for user input

--- projectfile.js
+++ projectfile.js
@@ -1,2 +1,27 @@
 //Access to the local file system
 const fs = require(`fs`)
+
+// Install the `inquirer` dependency HERE
+const inquirer = require(`inquirer`)
+
+//here we will create a function to generate a mark down of the README.MD
+function generateMarkDown(data) {
+return ` # ${data.title}`;
+}
+
+// Create a function that returns a license badge based on which license is passed in
+function renderLicenseBadge(license) { }
+
+//  Create a function that returns the license link
+function renderLicenseLink(license) { }
+
+
+//  Create a function that returns the license section of README
+function renderLicenseSection(license) { }
+
+// Have to create a promt for user input 
+const questions = [{
+    type: `input`,
+    name: `uaername`,
+
+}]
