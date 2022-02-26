9d7239c955185796507540597961a79dda08dc77
Carlos Rivas <Cjrivas00@gmail.com>
2022-02-25 22:51:34 -0500

    updated bugs finished input questions will need to call them and add a function for readme to read the input data

--- projectfile.js
+++ projectfile.js
@@ -33,7 +33,8 @@ const questions = [{
     }
 }, 
 
-{
+{ 
+    // This is input for repo name 
     type: `input`,
     name: `repository`,
     message: `Enter the title of your project`,
@@ -46,6 +47,7 @@ const questions = [{
     }
 },
 {
+    // this is input descrption
     type: `iniput`,
     name: `Description`,
     message: `Enter the description of your project`,
@@ -57,13 +59,49 @@ const questions = [{
         return (true)
     }
 },
+
 {
+    // Explain how the user can install project if necessary
     type: `iniput`,
     name: `insallation`,
-    message: `Explain how the user can install your project `,
-   
-}
+    message: `Explain how user would install (if necessary) for Installation Section. `,  
+    // Validation not required if question is optional
+},
 
-
-
-]
+{
+    // Explain The usage for the project
+    type: `iniput`,
+    name: `usage`,
+    message: `Enter your project instructions and examples of it in use for Usage Section. `,  
+},
+{
+    // List of license
+    type: `list`,
+    name: `License`,
+    message: `Enter what Lincese you would like to have on your readme `,  
+    Choices: [	`afl-3.0`, `apache-2.0`, `artistic-2.0`, `bsl-1.0`, `bsd-2-clause`, `bsd-3-clause`, bsd-3-clause-clear, 'cc', 'cc0-1.0', 'cc-by-4.0', 'cc-by-sa-4.0', 'wtfpl', 'ecl-2.0', 'epl-1.0', 'epl-2.0', 'eupl-1.1', 'agpl-3.0', 'gpl', 'gpl-2.0', 'gpl-3.0', 'lgpl', 'lgpl-2.1', 'lgpl-3.0', 'isc', 'lppl-1.3c', 'ms-pl', 'mit', 'mpl-2.0', 'osl-3.0', 'postgresql', 'ofl-1.1', 'ncsa', 'unlicense', 'zlib' ]
+},
+{
+    type: `input`,
+    name: `contributing`,
+    message: 'Explain how users can contribute to your project (if necessary).',
+     // Validation not required if q is optional
+},
+{
+    type: 'input',
+    name: 'tests',
+    message: 'Provide tests for project, and explain how to test (if necessary).',
+    // Validation not required if q is optional
+},
+{
+    type: 'input',
+    name: 'email',
+    message: ' Please Provide an Email',
+    function (answer){
+        if (answer.length <1){
+        return console.log(`Please provide a valid email`)
+        }
+        return(true)
+    }
+},
+];
