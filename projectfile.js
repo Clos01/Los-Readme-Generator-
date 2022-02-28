b454e2db7b81c10a1e1db7c428bf8cdbd617ee03
Carlos Rivas <Cjrivas00@gmail.com>
2022-02-27 21:22:00 -0500

    final project had to work on so many bugs and misspelled words

--- projectfile.js
+++ projectfile.js
@@ -9,99 +9,159 @@ function generateMarkDown(data) {
 return ` # ${data.title}`;
 }
 
-// Create a function that returns a license badge based on which license is passed in
-function renderLicenseBadge(license) { }
-
-//  Create a function that returns the license link
-function renderLicenseLink(license) { }
-
-
-//  Create a function that returns the license section of README
-function renderLicenseSection(license) { }
+ 
 
 // Have to create a promt for user input 
-const questions = [{
-    type: `input`,
-    name: `uaername`,
-    message: `Enter your Github username`,
-    // will have to validate that they enter one word 
-    valdiate: function (answer) {
-        if (answer.length <1){
-            return console.log(`please enter a Github UserName`)
+const Questions = [{
+    type: 'input',
+    name: 'github',
+    message: 'Enter your GitHub username.',
+    // validate that user entered at least one word
+    // https://stackoverflow.com/questions/57321266/how-to-test-inquirer-validation
+    validate: function (answer) {
+        if (answer.length < 1) {
+            return console.log("You must enter a GitHub username.");
         }
-        return (true)
-    }
-}, 
-
-{ 
-    // This is input for repo name 
-    type: `input`,
-    name: `repository`,
-    message: `Enter the title of your project`,
-    // will have to validate that they entered something cannot be blank
-    valdiate: function (answer) {
-        if (answer.length <1 ){
-            return console.log(`Please Enter the title of your project!`)
-        }
-        return (true)
-    }
-},
-{
-    // this is input descrption
-    type: `iniput`,
-    name: `Description`,
-    message: `Enter the description of your project`,
-    // will have to validate that they entered something cannot be blank 
-    valdiate: function (answer){
-        if (answer.length <1 ){
-            return console.log(`Please Enter a description of your project!`)
-        }
-        return (true)
+        return true;
     }
 },
 
-{
-    // Explain how the user can install project if necessary
-    type: `iniput`,
-    name: `insallation`,
-    message: `Explain how user would install (if necessary) for Installation Section. `,  
-    // Validation not required if question is optional
-},
-
-{
-    // Explain The usage for the project
-    type: `iniput`,
-    name: `usage`,
-    message: `Enter your project instructions and examples of it in use for Usage Section. `,  
-},
-{
-    // List of license
-    type: `list`,
-    name: `License`,
-    message: `Enter what Lincese you would like to have on your readme `,  
-    Choices: [	`afl-3.0`, `apache-2.0`, `artistic-2.0`, `bsl-1.0`, `bsd-2-clause`, `bsd-3-clause`, bsd-3-clause-clear, 'cc', 'cc0-1.0', 'cc-by-4.0', 'cc-by-sa-4.0', 'wtfpl', 'ecl-2.0', 'epl-1.0', 'epl-2.0', 'eupl-1.1', 'agpl-3.0', 'gpl', 'gpl-2.0', 'gpl-3.0', 'lgpl', 'lgpl-2.1', 'lgpl-3.0', 'isc', 'lppl-1.3c', 'ms-pl', 'mit', 'mpl-2.0', 'osl-3.0', 'postgresql', 'ofl-1.1', 'ncsa', 'unlicense', 'zlib' ]
-},
-{
-    type: `input`,
-    name: `contributing`,
-    message: 'Explain how users can contribute to your project (if necessary).',
-     // Validation not required if q is optional
-},
 {
     type: 'input',
-    name: 'tests',
+    name: 'repository',
+    message: 'Enter the name of your repository on GitHub.',
+    // We need to validate that user entered at least one word
+    validate: function (answer) {
+        if (answer.length < 1) {
+            return console.log("You must enter the name of your GitHub repository.");
+        }
+        return true;
+    }
+},
+
+{
+    type: 'input',
+    name: 'title',
+    message: 'Enter the title of your project.',
+    // validate that user entered at least one word
+    validate: function (answer) {
+        if (answer.length < 1) {
+            return console.log("You must enter the title of your project.");
+        }
+        return true;
+    }
+},
+
+
+
+{
+    type: 'input',
+    name: 'description',
+    message: 'Enter a description of your project.',
+    // We need to validate that user entered at least one word
+    validate: function (answer) {
+        if (answer.length < 1) {
+            return console.log("You must enter a description for your project.");
+        }
+        return true;
+    }
+},
+// Project Installation
+{
+    type: 'input',
+    name: 'installation',
+    message: 'Explain how user would install (if necessary) for Installation Section.',
+    // Validation not required if question is optional
+},
+// Usage of project
+{
+    type: 'input',
+    name: 'usage',
+    message: 'Enter your project instructions and examples of it in use for Usage Section.',
+},
+// Select license
+{
+    type: 'list',
+    name: 'license',
+    message: 'Choose your license for your project.',
+    // https://docs.github.com/en/free-pro-team@latest/github/creating-cloning-and-archiving-repositories/licensing-a-repository
+    choices: ['afl-3.0', 'apache-2.0', 'artistic-2.0', 'bsl-1.0', 'bsd-2-clause', 'bsd-3-clause', 'bsd-3-clause-clear', 'cc', 'cc0-1.0', 'cc-by-4.0', 'cc-by-sa-4.0', 'wtfpl', 'ecl-2.0', 'epl-1.0', 'epl-2.0', 'eupl-1.1', 'agpl-3.0', 'gpl', 'gpl-2.0', 'gpl-3.0', 'lgpl', 'lgpl-2.1', 'lgpl-3.0', 'isc', 'lppl-1.3c', 'ms-pl', 'mit', 'mpl-2.0', 'osl-3.0', 'postgresql', 'ofl-1.1', 'ncsa', 'unlicense', 'zlib']
+
+},
+// Contributing to project
+{
+    type: 'input',
+    name: 'contributions',
+    message: 'Explain how users can contribute to your project (if necessary).',
+    // Validation not required if q is optional
+},
+// Test for project
+{
+    type: 'input',
+    name: 'test',
     message: 'Provide tests for project, and explain how to test (if necessary).',
     // Validation not required if q is optional
 },
+
+
+
 {
     type: 'input',
     name: 'email',
-    message: ' Please Provide an Email',
-    function (answer){
-        if (answer.length <1){
-        return console.log(`Please provide a valid email`)
-        }
-        return(true)
-    }
+    message: 'What is your email address?'
 },
+
 ];
+// Create a function to write README file
+function writeToFile(input) {
+    var readMeText = `# ${input.title}
+##Table of Contents 
+[Description](#description)
+[Installation](#installation)
+[Usage](#usage)
+[License](#license)
+[Contributing](#contributing)
+[Testing](#testing)
+[Questions](#questions)
+## Description 
+${input.description}
+## Installation 
+${input.installation}
+## Usage
+${input.usage}
+## License
+${input.license}
+## Contributing
+${input.contributions}
+## Testing
+${input.test}
+## Questions 
+(${input.github})
+${input.email}`
+
+
+//https://nodejs.dev/learn/writing-files-with-nodejs refrenced documentation 
+
+fs.writeFile('./readme.md', readMeText, err => {
+    if (err) {
+        console.error(err)
+        return
+    }
+    //file written -ok
+})
+}
+
+
+// Reference: https://www.npmjs.com/package/util.promisify
+// TODO: Create a function to initialize app
+function init() {
+    inquirer.prompt(Questions)
+        .then((answers) => {
+            writeToFile(answers)
+
+        })
+
+}
+
+// Function call to initialize app
+init();
---
+++ readme.md
@@ -0,0 +1,24 @@
+# ascacas
+##Table of Contents 
+[Description](#description)
+[Installation](#installation)
+[Usage](#usage)
+[License](#license)
+[Contributing](#contributing)
+[Testing](#testing)
+[Questions](#questions)
+## Description 
+ascacasc
+## Installation 
+acasca
+## Usage
+cacasca
+## License
+afl-3.0
+## Contributing
+cascaca
+## Testing
+cascas
+## Questions 
+(scaasca)
+csacascacasc@gmail.com
